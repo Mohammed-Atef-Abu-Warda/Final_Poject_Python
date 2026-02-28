2e21b770ce9ffaaaa36aa636656c7f4139998b88
Mohammed-Atef-Abu-Warda <mohammedatefabuwarda229@gmail.com>
2026-02-28 15:08:23 +0200

    Update settings.py

--- library_management/settings.py
+++ library_management/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-rg08rhl&8kt(low2az31q(t=j!)pen-)abfe&(sfu6u1=b3m+z
 
 
 # الخيار الأسهل والأضمن لكل المنصات:
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
 # إعدادات الملفات الثابتة (Static Files)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
